8e84c93b46cddb96e6e5f27e1f88fe71eb2c4fff
laura-went <laurawent@hotmail.com>
2021-06-01 01:16:00 +0200

    Minor update tests

--- tests/test_optimiser.py
+++ tests/test_optimiser.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from plot_optimiser import PlotOptimiser
 from plot_axes_optimiser import PlotAxesOptimiser
 from plot_options_optimiser import PlotOptionsOptimiser
+import os.path
+from os import path
 
 dummy_input_path = 'tests/dummy_data/penguins.csv'
 dummy_options_path = "[[['species', 'flipper_length_mm', 'sex'], 'violinplot'],[['species', 'bill_length_mm'], 'barplot']]"
@@ -27,9 +29,9 @@ def test_plot():
     data = plot_optimiser.decide_plot()
     paths = ['island-species_plot.png', 'bill_length_mm-species_plot.png', 'bill_depth_mm-species_plot.png',
              'flipper_length_mm-species_plot.png', 'body_mass_g-species_plot.png', 'sex-species_plot.png']
-    for path in paths:
-        assert path.exists(Path(path)) is True
-        remove(Path(path))
+    for path_r in paths:
+        assert path.exists(path_r) is True
+        remove(path_r)
 
 
 
@@ -37,15 +39,15 @@ def test_plot_options():
     data = options_optimiser.decide_plot()
     paths = ['1_species_violinplot.png', '2_species_barplot.png']
 
-    for path in paths:
-        assert path.exists(Path(path)) is True
-        remove(Path(path))
+    for path_r in paths:
+        assert path.exists(path_r) is True
+        remove(path_r)
 
 
 def test_plot_axes():
     data = axes_optimiser.decide_plot()
     paths = ['sns_plot.png']
 
-    for path in paths:
-        assert path.exists(Path(path)) is True
-        remove(Path(path))
+    for path_r in paths:
+        assert path.exists(path_r) is True
+        remove(path_r)
