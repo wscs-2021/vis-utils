0f24d07e034ae376c3f4916dd5212145ad811a63
Misha Rigot <misharigot@gmail.com>
2021-06-03 23:36:16 +0200

    snake_case package name. 'None' string option.

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+test_executable: test_plot_executable test_options_executable test_axes_executable
+
 test_plot_executable:
 	INPUT_PATH="tests/dummy_data/penguins.csv" src/run.py plot;
 
@@ -6,3 +8,6 @@ test_options_executable:
 
 test_axes_executable:
 	INPUT_PATH="tests/dummy_data/penguins.csv" X_AXIS="species" OUTPUT_PATH="tests" src/run.py plot;
+
+test_none_arg_executable:
+	INPUT_PATH="tests/dummy_data/penguins.csv" X_AXIS="None" Y_AXIS="None" OPTIONS="None" OUTPUT_PATH="None" src/run.py plot;
--- src/run.py
+++ src/run.py
@@ -8,43 +8,47 @@ import yaml
 
 from plot_optimiser import PlotOptimiser
 
+
 def plot(input_path, x_axis, y_axis, hue_axis, options, output_path):
-  optimiser = PlotOptimiser(
-    input_path=input_path,
-    x_axis=x_axis,
-    y_axis=y_axis,
-    hue_axis=hue_axis,
-    options=options,
-    output_path=output_path
-  )
-  saved_space: str = optimiser.decide_plot()
-  return saved_space
+    optimiser = PlotOptimiser(
+        input_path=input_path,
+        x_axis=x_axis,
+        y_axis=y_axis,
+        hue_axis=hue_axis,
+        options=options,
+        output_path=output_path
+    )
+    saved_space: str = optimiser.decide_plot()
+    return saved_space
+
 
 def get_env_var(key: str) -> Union[str, None]:
-  """Returns environment variable by key. 
-  If the value is an empty string, it returns None instead.
+    """Returns environment variable by key. 
+    If the value is an empty string, it returns None instead.
 
-  Args:
-      key (str): env var key
+    Args:
+        key (str): env var key
 
-  Returns:
-      Union[str, None]: value of the env var
-  """
-  result = os.environ.get(key, None)
-  if result == "" or result == "\"\"":
-    return None
-  return result
+    Returns:
+        Union[str, None]: value of the env var
+    """
+    result = os.environ.get(key, None)
+    if result == "None" or result == "" or result == "\"\"":
+        return None
+    return result
+
 
 if __name__ == "__main__":
-  command = sys.argv[1]
-  input_path = get_env_var("INPUT_PATH")
-  options = get_env_var("OPTIONS")
-  x_axis = get_env_var("X_AXIS")
-  y_axis = get_env_var("Y_AXIS")
-  hue_axis = get_env_var("HUE_AXIS")
-  output_path = get_env_var("OUTPUT_PATH")
-  functions = {
-    "plot": plot,
-  }
-  output = functions[command](input_path, x_axis, y_axis, hue_axis, options, output_path)
-  print(yaml.dump({"output": output}))
+    command = sys.argv[1]
+    input_path = get_env_var("INPUT_PATH")
+    options = get_env_var("OPTIONS")
+    x_axis = get_env_var("X_AXIS")
+    y_axis = get_env_var("Y_AXIS")
+    hue_axis = get_env_var("HUE_AXIS")
+    output_path = get_env_var("OUTPUT_PATH")
+    functions = {
+        "plot": plot,
+    }
+    output = functions[command](
+        input_path, x_axis, y_axis, hue_axis, options, output_path)
+    print(yaml.dump({"output": output}))
